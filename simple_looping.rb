a9e9a01db5ea846a38e315f3ca9c015f33ca30e8
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-16 18:10:26 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -61,10 +61,14 @@ def until_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
-    
-    
+    until counter == 7
+      puts phrase
+      counter += 1
+    end
 
 end
+
+
 
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
