06a6c7e179b1e7e83bde339030ca9421d7a42f99
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-16 18:14:13 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -30,7 +30,7 @@ def times_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-    loop 7.times do
+    7.times do
      puts phrase
       break
     end
